a4c1a0e9ebc83d5ab61a4d2a662d8b888520c081
Roxana Mihalic <r.mihalic@netmatch.ro>
2021-12-06 16:41:43 +0200

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Cloud.PubSub.V1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GooglePubSubPublisher.Controllers
+{
+    [ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		public WeatherForecastController()
+		{
+		}
+
+		[HttpGet]
+		public async Task<int> Get()
+		{
+            return await PublishMessagesAsync("nmt-infra-development", "test-reviews", new List<string>() { "Test 3", "Test 4" });
+		}
+
+        public async Task<int> PublishMessagesAsync(string projectId, string topicId, IEnumerable<string> messageTexts)
+        {
+            TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
+            PublisherClient publisher = await PublisherClient.CreateAsync(topicName);
+
+            int publishedMessageCount = 0;
+            var publishTasks = messageTexts.Select(async text =>
+            {
+                try
+                {
+                    string message = await publisher.PublishAsync(text);
+                    Console.WriteLine($"Published message {message}");
+                    Interlocked.Increment(ref publishedMessageCount);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"An error ocurred when publishing message {text}: {exception.Message}");
+                }
+            });
+            await Task.WhenAll(publishTasks);
+            return publishedMessageCount;
+        }
+    }
+}
